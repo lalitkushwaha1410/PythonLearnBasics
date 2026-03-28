7ae3ab7eb323e08b3c8a53e6329dd3c9c93519ca
Lalit Kushwaha <lalitkushwaha1410@gmail.com>
2026-03-29 00:57:01 +0530

    added formatOutput

--- Day_1_BasicPrint/conditionalStmt.py
+++ Day_1_BasicPrint/conditionalStmt.py
@@ -8,6 +8,7 @@ if age>=18:
 else:
     print("Not Eligible to Vote")
 
+if age>=18:print("Eligible to Vote")
 # if elif else
 marks =60
 print("Marks obtained :", marks)
@@ -22,3 +23,23 @@ else:
     print("Fail");
 
 # nested elif
+
+#short hand if else
+a,b = 20,50
+print("a is greater") if a>b else print("b is greater")  #ternary operator
+
+# match Case statement (similar to switch case)
+day =3;
+match day:
+    case 1:print("Sunday")
+    case 2:print("Monday")
+    case 3:print("Tuesday")
+    case 4:print("Wednesday")
+    case _:print("Not Valid Day number")
+
+num =20
+match num:
+    case 2|3|4|5|6:print("Weekday")
+    case 1|7:print("Weekends")
+    case _:print("Not valid day number")
+
