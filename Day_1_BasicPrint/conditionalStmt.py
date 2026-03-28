a7bfd7989badbc9baf8a84d896b509606b94c9a3
Lalit Kushwaha <lalitkushwaha1410@gmail.com>
2026-03-28 11:53:12 +0530

    added formatOutput

---
+++ Day_1_BasicPrint/conditionalStmt.py
@@ -0,0 +1,22 @@
+#example-1
+from traceback import print_tb
+
+age=20
+
+if age>=18:
+    print("Eligible to Vote")
+else:
+    print("Not Eligible to Vote")
+
+# if elif else
+marks =60
+print("Marks obtained :", marks)
+
+if marks>90:
+    print("Grade A")
+elif marks>70:
+    print("Grade B")
+elif marks>50:
+    print("Grade C")
+else:
+    print("Fail");
--- Day_1_BasicPrint/formatingOutput.py
+++ Day_1_BasicPrint/formatingOutput.py
@@ -1,23 +1,26 @@
-name , age ,sal = "john",25,50000.00
+name, age, sal = "john", 25, 50000.00
 
 #approach-1
-print(name,age,sal);
+print(name, age, sal);
 
 #approach-2
-print("Name :"+name); #+ used for string concatenation
-print("Name :",name); # , is also used for concate
-print("Age :" , age); # , is used to concate number values
+print("Name :" + name);  #+ used for string concatenation
+print("Name :", name);  # , is also used for concate
+print("Age :", age);  # , is used to concate number values
 
 #approach-3
 # %s--string , %d--->int   %g---> decimal
-print("Name:%s  Age:%d  Salary:%g" %(name,age,sal));
+print("Name:%s  Age:%d  Salary:%g" % (name, age, sal));
 
 #approach-4
 
-print("Name:{} Age:{} Salary:{}".format(name,age,sal))
+print("Name:{} Age:{} Salary:{}".format(name, age, sal))
 
 #approach-5
-print("Name:{0} Age:{1} Salary:{2}".format(name,age,sal))
-print("Salary:{2} Age:{1} Name:{0} ".format(name,age,sal))
+print("Name:{0} Age:{1} Salary:{2}".format(name, age, sal))
+print("Salary:{2} Age:{1} Name:{0} ".format(name, age, sal))
 
 #approach-6 : print data in multiple lines
+
+print(" Welcome to \npython world")
+print(" Welcome to \tpython world")
