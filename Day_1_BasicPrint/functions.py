4becbdd5b9d44f968a5157a30fe392a626029c93
Lalit Kushwaha <lalitkushwaha1410@gmail.com>
2026-04-06 20:24:26 +0530

    list and functions

--- Day_1_BasicPrint/functions.py
+++ Day_1_BasicPrint/functions.py
@@ -14,4 +14,29 @@ def namefun(name):
 
 namefun("Peter")  # peter is argument
 
+#example :
+def add(a,b):
+    return a+b
 
+sum = add(5,10)
+print("@@@@@@@@@@@@@@@@@@@@@@",sum)
+
+#example : arbitrary arguments : n number of arguments
+def sum_function(*num):
+    total =0
+    for i in num:
+        total =total +i
+    return total
+
+print(sum_function(10,20,30))
+print(sum_function(50,60))
+print(sum_function(4,6,7,8,9,2))
+
+# Keyword and positional arguments
+def myfun(a,b,c):
+    print(a,b,c)
+
+myfun(10,20,30) # positinal arguements
+
+myfun(c=30,a=50,b=80)
+myfun(20,70,c=40) # position argument must come before keyword argument
