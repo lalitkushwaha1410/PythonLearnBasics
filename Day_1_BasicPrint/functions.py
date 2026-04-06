25fcfcc07db8845fb0bda418206114dceb420a20
Lalit Kushwaha <lalitkushwaha1410@gmail.com>
2026-04-06 17:56:28 +0530

    list and functions

---
+++ Day_1_BasicPrint/functions.py
@@ -0,0 +1,17 @@
+# diff bet function and method
+# group of statements which performs certain task , avoid code duplication , reusability
+# functions created inside Class , they are called as Methods
+# Def keyword is used to create a function in python.
+# 1. No argument 2. parameterized function
+
+def myfun():
+    print("hello")
+
+myfun() # function call
+
+def namefun(name):
+    print("Hello", name)  # name is parameter
+
+namefun("Peter")  # peter is argument
+
+
--- Day_1_BasicPrint/list.py
+++ Day_1_BasicPrint/list.py
@@ -1,2 +1,20 @@
-# a list is a collection which is ordered and changebale.
+# a list is a collection which is ordered and changeable.
 # in python lists are written with square brackets [].
+
+list1 = [10,20,30,40]
+list2 = [10, "apple", 30, "mango"]
+
+mylist3 = list()
+print(list2)
+print(mylist3)
+print(list2[-1]) # from 1st one from end
+print(list2[2]) # from begin , starts with zero index , from end starts from -1
+
+fruits = ["apple", "mango","apple","banana", "cherry", "lemon", "kiwi", "orange"]
+print(fruits[2:5])
+print("**********************************************************")
+for i in fruits:
+    print(i)
+
+print(len(fruits))
+print(fruits.count("apple"))
